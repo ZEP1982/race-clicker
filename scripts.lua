385b2c39286590b1de77ad4efaa89fc007cdd808
ZEP1982 <90557635+ZEP1982@users.noreply.github.com>
2023-01-28 23:51:19 +0800

    Create scripts.lua

---
+++ scripts.lua
@@ -0,0 +1,13 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+
+local Window = Library.CreateLib("SeaHub | PMEX","BloodTheme")
+
+-- Tabs
+
+local Tab1 = Window:NewTab("Troll")
+
+-- Section 
+
+local _ = Tab1:NewSection("h")
+
+_:NewSlider
